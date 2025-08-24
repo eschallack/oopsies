f77eb4670943a2aa92148ab711472cbc0b163dec
eschallack <jdean1134@gmail.com>
2025-08-24 10:21:58 -0500

    First working prototype woohoo!

--- oopsies/faker_sqlalchemy.py
+++ oopsies/faker_sqlalchemy.py
@@ -52,47 +52,22 @@ releasing support for python 3.11.
 import inspect as insp
 import datetime
 from typing import Any, TypeVar, Type, Dict, Union, List, Callable
-from column_mapping_engine import rank
+from oopsies.column_mapping_engine import rank
 from faker import Faker
+from faker.proxy import Faker as FakerProxy
 from faker.providers import BaseProvider
 from faker.providers.date_time import Provider as DateTimeProvider
 from faker.providers.misc import Provider as MiscProvider
 from faker.providers.python import Provider as PythonProvider
-import faker.proxy
 from sqlalchemy.orm import Mapper, RelationshipProperty
 try:
     from sqlalchemy.orm import DeclarativeMeta
 except ImportError:
     from sqlalchemy.ext.declarative import DeclarativeMeta
 from sqlalchemy.sql.type_api import TypeEngine
-from sqlalchemy import (
-    inspect,
-    Column,
-    ARRAY,
-    BigInteger,
-    BINARY,
-    Boolean,
-    CLOB,
-    Date,
-    DateTime,
-    Enum,
-    Float,
-    Integer,
-    Interval,
-    JSON,
-    LargeBinary,
-    Numeric,
-    SmallInteger,
-    Unicode,
-    UnicodeText,
-    String,
-    Time
-)
-from sqlalchemy.sql.sqltypes import (
-    BOOLEAN,
-    VARCHAR)
-from faker import Generator
-
+from sqlalchemy import (inspect,Column,ARRAY,BigInteger,BINARY,Boolean,CLOB,Date,DateTime,Enum,Float,Integer,Interval,JSON,LargeBinary,Numeric,SmallInteger,Unicode,UnicodeText,String,Time)
+from sqlalchemy.sql.sqltypes import (BOOLEAN,VARCHAR,NullType,UUID,ARRAY,BIGINT,DATE,DATETIME,CHAR,DECIMAL,FLOAT,INTEGER,NCHAR,SMALLINT,TIMESTAMP,REAL,DOUBLE_PRECISION)
+from typing import get_type_hints
 __version__ = "0.10.2208140"
 __all__ = (
     "SqlAlchemyProvider",
@@ -106,54 +81,16 @@ GeneratorFunction = Callable[
 ]
 GeneratorSpec = Union[str, GeneratorFunction]
 
-
 def _generate_date(generator: DateTimeProvider, _: Any) -> datetime.date:
     return generator.date_time().date()
 def _generate_date_time(generator: DateTimeProvider, _: Any) -> datetime.date:
     return generator.date_time()
-
-
 def _generate_time(generator: DateTimeProvider, _: Any) -> datetime.time:
     return generator.date_time().time()
-
-
-def _generate_json(generator: PythonProvider, _: Any) -> Dict[str, Union[str, int, bool, List[PrimitiveJsonTypes], Dict[str, PrimitiveJsonTypes]]]:
-    return generator.pydict(
-        10,
-        True,
-        [
-            str,
-            int,
-            bool,
-        ],
-    )
-def _generate_small_bytes(generator: MiscProvider, _: Any) -> bytes:
-    return generator.binary(100)
 def _generate_uuid(generator: MiscProvider, _: Any) -> bytes:
     return generator.uuid4()
 def _generate_null(generator: MiscProvider, _: Any) -> bytes:
     return None
-from sqlalchemy.sql.sqltypes import (
-    BOOLEAN,
-    VARCHAR,
-    NullType,
-    UUID,
-    ARRAY,
-    BIGINT,
-    DATE,
-    DATETIME,
-    CHAR,
-    DECIMAL,
-    FLOAT,
-    INTEGER,
-    NCHAR,
-    SMALLINT,
-    TIMESTAMP,
-    REAL,
-    DOUBLE_PRECISION
-    )
-from typing import get_type_hints
-
 
 DEFAULT_MAPPINGS: Dict[TypeEngine, GeneratorSpec] = {
     BigInteger: int,
@@ -225,7 +162,7 @@ class SqlAlchemyProvider(BaseProvider):
             results[name] = ret_type
         return results
     
-    def get_type_to_faker_methods_recordset(self) -> list[Any]:
+    def _get_type_to_faker_methods_recordset(self) -> list[Any]:
         """Gets All provider methods from faker proxy, and returns a recordset of {type1:[methodname1,etc]...}
 
         :return: _description_
@@ -247,19 +184,18 @@ class SqlAlchemyProvider(BaseProvider):
         """Scan the Faker proxy for all provider methods and capture their return types."""
         instance = cls()
         results = {}
+        exclude_attrs = ['add_provider']
         for attr in dir(instance):
-            if attr.startswith("_"):
+            if attr.startswith("_") or attr in exclude_attrs:
                 continue
             try:
                 member = getattr(instance, attr)
             except TypeError:
                 continue
             if insp.isfunction(member) or insp.ismethod(member):
-                print('yay')
                 try:
                     hints = get_type_hints(member)
                     ret_type = hints.get("return", insp.signature(member).return_annotation)
-                    print(ret_type)
                 except Exception:
                     ret_type = insp.signature(member).return_annotation
                 if ret_type is insp._empty:
@@ -274,10 +210,10 @@ class SqlAlchemyProvider(BaseProvider):
     
         assert isinstance(model, DeclarativeMeta)
         assert not (generate_primary_keys and generate_related), "`generate_primary_keys` and `generate_related` " \
-                                                                 "MUST NOT both be set to True"
+                                                              "MUST NOT both be set to True"
         self.inspection: Mapper = inspect(model)
+        self.faker_instance = Faker()
         self.method_map =self.scan_base_class(Faker)
-
         values = {}
         for column in self.inspection.columns:
             if column.name in overrides:
@@ -294,7 +230,7 @@ class SqlAlchemyProvider(BaseProvider):
                     generate_primary_keys=generate_primary_keys, 
                     generate_related=True
                 )
-
+        
         return model(**values)
 
     def sqlalchemy_column_value(self, column: Column) -> ColumnType:
@@ -304,52 +240,60 @@ class SqlAlchemyProvider(BaseProvider):
         :return: Returns a value that may be assigned to ``Column`` attributes.
         """
 
-        generator_spec = self._find_generator_spec(column)
+        generator_spec, kwargs = self._find_generator_spec(column)
 
         if callable(generator_spec):
-            return generator_spec(self.generator, column)
+            return generator_spec(**kwargs)
         else:
-            return self._find_generator(generator_spec)()
-    def _get_col_type(self, column:Column):
-        if hasattr(column.type, 'python_type'):
-            if column.type.python_type:
-                dtype=column.type.python_type
-        
-        elif hasattr(column.type, 'data_type'):
-            if column.type.data_type:
-                dtype=column.type.data_type
-        else:
-            dtype=column.type
-        return dtype
-    def _get_method_meta(self, method:Callable):
-        return {""}
-    def _find_generator_spec(self, column: Column):
-        dtype = self._get_col_type(column)
+            return None
+
+    def _get_col_type(self,column:Column):
+        t = column.type
+        if isinstance(t, TypeEngine):
+            try:
+                return t.python_type   # the canonical way
+            except NotImplementedError:
+                pass
+        try:
+            if hasattr(t, 'data_type'):
+                t=t.data_type
+            return t._type_affinity().python_type
+        except Exception:
+            return None
+    def _create_method_name_map_with_provider_name(self, methods:list[Callable]) -> None:
+        self.method_name_map = {}
+        for method in methods:
+            classname=method.__self__.__class__.__name__.replace('Provider','')
+            if classname != '':
+                metaname=f"{method.__class__} {method.__name__}"
+            else:
+                metaname=method.__name__
+            self.method_name_map[metaname]=method.__name__
+    def _rank_method_column_cosine_similarity(self, methods:list[Callable], column:Column) -> list[Callable]:
+        self._create_method_name_map_with_provider_name(methods)
+        return rank(list(self.method_name_map.keys()),column.name)
+    def _filter_methods_by_return_type(self, dtype) -> list[str|None]:
         method_list = []
         for k, v in self.method_map.items():
             if v == dtype:
                 method_list.append(k)
         if not isinstance(method_list, list):
             return None
-        method_list = [method for method in method_list if isinstance(method, Callable)]
-        if len(method_list) == 0:
-            return None
-        elif len(method_list) == 1:
-            return method_list[0]
+        return [method for method in method_list if isinstance(method, Callable)]
+    def _find_generator_spec(self, column: Column):
+        # first, we should handle values that are effectively enums.
+        # we need to identify all types which effectively function like enums
+        return_type = self._get_col_type(column)
+        methods=self._filter_methods_by_return_type(return_type)
+        if len(methods) == 0:
+            method=None
+        elif len(methods) == 1:
+            method=methods[0]
         else:
-            method_name_map = {}
-            
-            for v in method_list:
-                classname=v.__self__.__class__.__name__.replace('Provider','')
-                if classname != '':
-                    name=f"{v.__class__} {v.__name__}"
-                else:
-                    name=v.__name__
-                method_name_map[name]=v
-            ranks=rank(list(method_name_map.keys()),column.name)
-            if ranks[1] < 0.75:
-                pass
-            print(method_name_map)
+            ranks = self._rank_method_column_cosine_similarity(methods,column)
+            method_name=self.method_name_map[ranks[0][0]]
+            method = getattr(self.faker_instance, method_name)
+        return method, {}
         # raise ValueError(f"Unmapped column type found for column: {column}")
 
     def _find_generator(self, generator_spec):
@@ -357,28 +301,5 @@ class SqlAlchemyProvider(BaseProvider):
             return getattr(self.generator, generator_spec)
         else:
             return getattr(self, generator_spec)
-if __name__ == '__main__':
-    from sqlalchemy import create_engine, MetaData, Table
-    from sqlalchemy.orm import registry
-    fake = Faker()
-    fake.add_provider(SqlAlchemyProvider)
-    # Connect to the foreign DB
-    engine = create_engine("postgresql+psycopg2://postgres:postgres@localhost:5432/Adventureworks")
 
-    metadata_obj = MetaData()
-
-    person = Table(
-        "person",
-        metadata_obj,
-        autoload_with=engine,
-        schema='person'
-    )
-    from sqlalchemy.orm import declarative_base
-
-    Base = declarative_base()
-    class Person(Base):
-        __table__ = person
-        Base
-    instance = fake.sqlalchemy_model(Person)
-    v = instance.value
-    print(v)
+    
